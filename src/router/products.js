a3491d148b4a177f83bc3da041fefc2fb0a6b77f
Tim Daff <tim@southerndigital.com.au>
2022-05-24 16:40:19 +1000

    Removed hash from urls

--- src/router/products.js
+++ src/router/products.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import Product from '../views/Product.vue';
 import Products from '../views/Products.vue';
 
@@ -17,7 +17,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
